a80caf81fdf9cd41b2f66065442ec28dfdd4f3d3
NotJem <46477254+ccakmak60@users.noreply.github.com>
2024-02-17 22:05:16 -0600

    Update nodes_mappings.py

--- src/nodes_mappings.py
+++ src/nodes_mappings.py
@@ -3,11 +3,12 @@ from .nodes.save_image_s3 import SaveImageS3
 from .nodes.save_video_files_s3 import SaveVideoFilesS3
 from .nodes.download_file_s3 import DownloadFileS3
 from .nodes.upload_file_s3 import UploadFileS3
-from .nodes.laod_checkpoint.py import CheckpointLoader
+from .nodes.download_checkpoint_s3 import DownloadCheckpointS3
 
 
 
 NODE_CLASS_MAPPINGS = {
+    "DownloadCheckpointS3": DownloadCheckpointS3,
     "LoadImageS3": LoadImageS3,
     "SaveImageS3": SaveImageS3,
     "SaveVideoFilesS3": SaveVideoFilesS3,
@@ -17,6 +18,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "DownloadCheckpointS3": "Download Checkpoint from S3",
     "LoadImageS3": "Load Image from S3",
     "SaveImageS3": "Save Image to S3",
     "SaveVideoFilesS3": "Save Video Files to S3",
